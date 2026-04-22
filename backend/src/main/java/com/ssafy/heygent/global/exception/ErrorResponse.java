f0b9ae14e235878618b249d8c673a002f8fe5aa2
전희수 <acesk123@naver.com>
2026-04-22 12:21:35 +0900

    BE-chore : 공통 예외 처리 규약 추가

---
+++ backend/src/main/java/com/ssafy/heygent/global/exception/ErrorResponse.java
@@ -0,0 +1,26 @@
+package com.ssafy.heygent.global.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+    private final int status;
+    private final String error;
+    private final String code;
+    private final String message;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+            .status(errorCode.getStatus())
+            .body(ErrorResponse.builder()
+                .status(errorCode.getStatus().value())
+                .error(errorCode.getStatus().name())
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .build());
+    }
+}
