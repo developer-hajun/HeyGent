d445f89e98c35175bda10c9409dd29fd66b4b32f
전희수 <acesk123@naver.com>
2026-04-22 11:26:35 +0900

    BE-chore: bootstrap backend and add AGENTS guide

---
+++ backend/src/main/java/com/ssafy/heygent/HeygentApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.heygent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeygentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeygentApplication.class, args);
+	}
+
+}
