163c6b1f72b9e9477eeaa92dd387d2fb9b668fb6
최석원 <seokwon1907@naver.com>
2026-04-23 12:27:18 +0900

    MOB-chore : 초기 세팅

---
+++ MOB/app/src/main/java/com/example/mob/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.mob
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.mob.ui.theme.MOBTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MOBTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Box(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .safeDrawingPadding(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text("test")
+    }
+}
