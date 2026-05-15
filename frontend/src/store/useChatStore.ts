d1c8aff858a53d4b36bb6bd4dc4a4de70607357e
이하준 <juni8828@naver.com>
2026-05-15 10:02:39 +0900

    FE-Feat : 앱작업 관련 작업 사항 문제 해결

--- frontend/src/store/useChatStore.ts
+++ frontend/src/store/useChatStore.ts
@@ -436,6 +436,9 @@ export const useChatStore = create<ChatState>((set, get) => ({
         }),
       }
     })
+    const sessionPanels = useSessionStore.getState().agentPanelsBySessionId[sessionId] ?? []
+    startVisualizationForSession(sessionId, taskRunId, sessionPanels)
+    refreshVisualizationSessionAgents(sessionId, taskRunId)
   },
   clearChatState: () =>
     set({
